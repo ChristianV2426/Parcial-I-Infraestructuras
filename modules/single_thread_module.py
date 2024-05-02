84f3bde8e90c32286d34309aa0c664f13b7234e3
Samuel Galindo <itzsaky@outlook.com>
2024-05-02 16:50:50 -0500

    Single thread implementation

--- modules/single_thread_module.py
+++ modules/single_thread_module.py
@@ -12,11 +12,37 @@
 
     Archivo: single_thread_module.py
 """
-
+import timeit
+from datetime import datetime
+from modules.reader import Reader
+from modules.script import script
 
 class SingleThreadModule:
     def __init__(self, videos_to_download, output_dir_path):
-        pass
+        self.videos_to_download = videos_to_download
+        self.total_videos = len(videos_to_download)
+
+        self.output_dir_path = output_dir_path
+        self.video_info = [_ for _ in range(self.total_videos)]
 
     def start(self):
-        pass
+        """
+        Inicia la ejecución del script. En este caso, la ejecución es secuencial.
+        Al final, imprime la información de los vídeos en un archivo JSON.
+        """
+        self.output_dir_path += f"/audio_extraction_at_{datetime.now().strftime('%d-%m-%Y__%H_%M_%S')}"
+
+        initial_time = timeit.default_timer()
+
+        for i in range(self.total_videos):
+            script(i, self.videos_to_download,
+                               self.output_dir_path, self.video_info)
+
+        total_time = timeit.default_timer() - initial_time
+
+        Reader.fix_and_save_json(self.output_dir_path, self.video_info)
+
+        print(
+            f"Audio extraction with a single thread, "
+            f"completed in {format(total_time, '.2f')} seconds / "
+            f"{format(total_time/60, '.2f')} minutes")
